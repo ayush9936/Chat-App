a63d46f382acae9d830b788bb05f7f90c886c76c
ayush9936 <ayush9936ru@gmail.com>
2024-09-16 16:48:46 +0530

    frontend is done

--- frontend/src/components/message/Message.jsx
+++ frontend/src/components/message/Message.jsx
@@ -8,6 +8,7 @@ const Message = () => {
           <img
             src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
             alt="user avatar"
+          
           />
         </div>
       </div>
